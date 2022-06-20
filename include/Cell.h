c3831d2223264ba1c5301912832366e0b73a9de1
progs2002 <pragatobhattacharjee@gmail.com>
2022-06-20 21:24:41 +0530

    added cells

---
+++ include/Cell.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include "constants.h"
+
+class Cell
+{
+public:
+    uint16_t row, col;
+    bool active;
+    SDL_Rect *rect;
+
+    Cell(uint16_t row, uint16_t col, SDL_Rect* rect);
+    Cell();
+    void changeState();
+};
---
+++ src/Cell.cpp
@@ -0,0 +1,21 @@
+#include "Cell.h"
+
+
+Cell::Cell(uint16_t row, uint16_t col, SDL_Rect* rect)
+: row(row), col(col), rect(rect)
+{
+    this->active = false;
+}
+
+Cell::Cell()
+: row(-1), col(-1)
+{
+    this->active = false;
+    this->rect = nullptr;
+}
+
+
+void Cell::changeState()
+{
+    this->active = !active;
+}
